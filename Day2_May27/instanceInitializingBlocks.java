555dda0b7e0127c7317393764a0217ba8fbbdf33
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-05-27 20:40:35 +0530

    Update instanceInitializingBlocks.java

--- Day2_May27/instanceInitializingBlocks.java
+++ Day2_May27/instanceInitializingBlocks.java
@@ -1,3 +1,14 @@
+/* Instance Initialization Blocks:
+Instance initialization blocks are enclosed within curly braces {} and are not associated with any specific method or constructor.
+They are executed whenever an object of the class is created, before the execution of any constructor.
+Instance initialization blocks are useful when you have common initialization code that needs to be executed for every object of a class.
+
+- In the example, the MyClass contains an instance initialization block enclosed within curly braces {}.
+Inside the block, we print a message and initialize the value variable to 10. The MyClass constructor is also present and prints a message.
+When we create an instance of MyClass (MyClass obj = new MyClass();), the instance initialization block is executed before the constructor.
+*/
+
+
 package Day2_May27;
 
 class MyClass{
