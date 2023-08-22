8647517cd05014871f9d2a77a4eb790c95ee8598
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-08-22 20:23:04 +0530

    Update rotateArray.java

--- Top - 150/rotateArray.java
+++ Top - 150/rotateArray.java
@@ -15,3 +15,37 @@ public class Solution {
 
     }
 }
+
+Time complexity - O(n)
+space  - O(n)
+
+
+    
+
+Optimal - 
+
+public class Solution {
+    public static void reverse(int nums[], int start, int end){
+    // While start index is less than end index
+    while(start < end){
+        // Swap elements at start and end indices
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        // Increment start index and decrement end index
+        start++;
+        end--;
+    }
+}
+
+    public void rotate(int[] nums, int k) {
+        // Ensure k is within array bounds
+        k %= nums.length;
+        // Reverse entire array
+        reverse(nums, 0, nums.length - 1);
+        // Reverse first k elements
+        reverse(nums, 0, k - 1);
+        // Reverse remaining elements
+        reverse(nums, k, nums.length - 1);
+    }
+}
