3c2dc17b386e435adeabb311ffb182b0413fbdeb
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-08-24 22:29:50 +0530

    Create jumpGame.java

---
+++ Top - 150/jumpGame.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach = 0;
+        for(int i = 0; i <= reach; i++) {
+            reach = Math.max(reach, i + nums[i]);
+            if(reach >= nums.length-1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+Time complexity = O(N)
+Space = O(1)
