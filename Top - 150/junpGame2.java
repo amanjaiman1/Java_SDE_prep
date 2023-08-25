791f9763e107bf6b3c5298e510d85a51b7367309
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-08-25 15:57:04 +0530

    Update junpGame2.java

--- Top - 150/junpGame2.java
+++ Top - 150/junpGame2.java
@@ -1,1 +1,23 @@
-Will try Later :)
+Not solved at my own #greedy
+
+class Solution {
+    public int jump(int[] nums) {
+        int curReach = 0;
+        int maxReach = 0; 
+        int count = 0;
+        int n = nums.length;
+        for(int i = 0; i < n-1; i++) {
+            if(i + nums[i] > maxReach) {
+                maxReach = i + nums[i];
+            }
+
+            if(i == curReach) {
+                count++;
+                curReach = maxReach;
+            }
+        }
+        return count;
+    }
+}
+
+Time complexity = O(n)
