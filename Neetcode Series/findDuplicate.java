571335626b50d39fe659dd7ebe606f48af4ff10d
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-08-25 22:30:47 +0530

    Update findDuplicate.java

--- Neetcode Series/findDuplicate.java
+++ Neetcode Series/findDuplicate.java
@@ -20,5 +20,21 @@ class Solution {
     }
 }
 
+By Hashmap
+Time complexity = O(N) || Space = O(N)
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                return true;
+            }
+            map.put(nums[i],1);
+        }
+
+        return false;
+    }
+}
 
 
