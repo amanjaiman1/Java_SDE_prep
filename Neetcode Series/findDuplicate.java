104d1a231dee167d514ba4735cfc2dbc15cf2282
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-08-25 22:29:20 +0530

    Update findDuplicate.java

--- Neetcode Series/findDuplicate.java
+++ Neetcode Series/findDuplicate.java
@@ -3,3 +3,20 @@ Basic Brute force - Nested Loops checking for the duplicate element
                       return true
 
 Time Complexity = O(N^2)
+
+2nd Brute - O(nlogn)
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i+1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+
+
