79f8126d99762222144f74a23d2cb7262a422516
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2023-11-18 19:57:53 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println("Hii, i'm just doing this for github Streak");
         System.out.println("Hii, i'm just doing this for github Streak");
         System.out.println("Hii, i'm just doing this for github Streak");
+        System.out.println("Hii, i'm just doing this for github Streak");
   }
 }
